b402e71f5853f71a9639be3723e2e69700866228
Rein Leen <rein@thebrickadmin.com>
2024-04-08 14:14:32 +0200

    Setup redux for users and categories

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 import { store } from "./store/store";
 
@@ -14,16 +12,12 @@ import "./index.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
+    <Provider store={store}>
+      <BrowserRouter>
           <CartProvider>
             <App />
           </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
